4e78f96bed17892e7b7f8252732e50376f46b781
mikeatlas <mikeatlas@gmail.com>
2014-07-21 11:58:00 -0400

    make loading zmq optional

--- lib/realtime/zmq_wrapper.rb
+++ lib/realtime/zmq_wrapper.rb
@@ -1,32 +1,34 @@
 class ZmqWrapper
 
- require 'em-zeromq'
+	if defined?(ZMQ) && defined?(EmZeromq)
+		require 'em-zeromq'
 
- def self.publish(the_message)
- 	EM.run {
-		zmq = EM::ZeroMQ::Context.new(1)
-		pusher = zmq.socket(ZMQ::PUSH)
-		pusher.connect($zmq_server)
-		message = "realtime_msg:" + the_message.to_json	
-		puts "Pushing realtime message: " + message
-		pusher.send_msg(message)
-		EM.stop
-	}
-  end
+		def self.publish(the_message)
+			EM.run {
+				zmq = EM::ZeroMQ::Context.new(1)
+				pusher = zmq.socket(ZMQ::PUSH)
+				pusher.connect($zmq_server)
+				message = "realtime_msg:" + the_message.to_json	
+				puts "Pushing realtime message: " + message
+				pusher.send_msg(message)
+				EM.stop
+			}
+		end
 
-  def self.store_session(user_id, session_id, session_data, expiration)
- 	EM.run {
-		zmq = EM::ZeroMQ::Context.new(1)
-		pusher = zmq.socket(ZMQ::PUSH)
-		pusher.connect($zmq_server)
-		the_message = {user_id: user_id, 
-					   session_id: session_id,
-					   session_data: session_data,
-					   expiration: expiration}
-		message = "rtSession:" + the_message.to_json	
-		puts "Pushing session message: " + message
-		pusher.send_msg(message)
-		EM.stop
-	}
-  end
+		def self.store_session(user_id, session_id, session_data, expiration)
+			EM.run {
+				zmq = EM::ZeroMQ::Context.new(1)
+				pusher = zmq.socket(ZMQ::PUSH)
+				pusher.connect($zmq_server)
+				the_message = {user_id: user_id, 
+					session_id: session_id,
+					session_data: session_data,
+					expiration: expiration}
+					message = "rtSession:" + the_message.to_json	
+					puts "Pushing session message: " + message
+					pusher.send_msg(message)
+					EM.stop
+				}
+		end
+	end
 end

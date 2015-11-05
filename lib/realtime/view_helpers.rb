ad162fba0eb4347ad47ff73a0e53a5220ee99a25
Ahmad Abdel-Yaman <a.yaman@gmail.com>
2015-11-05 19:13:45 +0200

    Switch the view helpers to the new Ruby hash notation

--- lib/realtime/view_helpers.rb
+++ lib/realtime/view_helpers.rb
@@ -1,29 +1,26 @@
 module Realtime
-	module ViewHelpers
+  module ViewHelpers
 
-		def realtime_support
-			return render(:template =>"realtime/realtime_support", 
-					:layout => nil, 
-					:locals => 
-					{ :realtime_token => @realtime_token,
-					  :realtime_domain => @realtime_domain,
-					  :realtime_server_url => @realtime_server_url,
-					  :realtime_user_id => @realtime_user_id }).to_s
-			
-		end
+    def realtime_support
+      return render(template: "realtime/realtime_support",
+                    layout: nil,
+                    locals:
+                    { realtime_token: @realtime_token,
+                      realtime_domain: @realtime_domain,
+                      realtime_server_url: @realtime_server_url,
+                      realtime_user_id: @realtime_user_id }).to_s
+    end
 
-		def realtime_message_handler
-			return render(:template =>"realtime/realtime_message_handler", 
-					:layout => nil, 
-					:locals => {}).to_s
-			
-		end
+    def realtime_message_handler
+      return render(template: "realtime/realtime_message_handler",
+                    layout: nil,
+                    locals: {}).to_s
+    end
 
-		def realtime_message_console_logger
-			return render(:template =>"realtime/realtime_message_console_logger", 
-					:layout => nil, 
-					:locals => {}).to_s
-			
-		end
-	end
+    def realtime_message_console_logger
+      return render(template: "realtime/realtime_message_console_logger",
+                    layout: nil,
+                    locals: {}).to_s
+    end
+  end
 end

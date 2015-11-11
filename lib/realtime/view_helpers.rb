02e07635aa1b4a57c592f9a8ef133bf2d619a2a7
Ahmad Abdel-Yaman <a.yaman@gmail.com>
2015-11-11 10:25:06 +0200

    Allow loading the message handler and the message console logger via options passed to 'realtime_support'

--- lib/realtime/view_helpers.rb
+++ lib/realtime/view_helpers.rb
@@ -1,26 +1,16 @@
 module Realtime
   module ViewHelpers
 
-    def realtime_support
+    def realtime_support(args = {})
       return render(template: "realtime/realtime_support",
                     layout: nil,
                     locals:
                     { realtime_token: @realtime_token,
                       realtime_domain: @realtime_domain,
                       realtime_server_url: @realtime_server_url,
-                      realtime_user_id: @realtime_user_id }).to_s
-    end
-
-    def realtime_message_handler
-      return render(template: "realtime/realtime_message_handler",
-                    layout: nil,
-                    locals: {}).to_s
-    end
-
-    def realtime_message_console_logger
-      return render(template: "realtime/realtime_message_console_logger",
-                    layout: nil,
-                    locals: {}).to_s
+                      realtime_user_id: @realtime_user_id,
+                      message_handler: args[:message_handler],
+                      message_console_logger: args[:message_console_logger] }).to_s
     end
   end
 end
